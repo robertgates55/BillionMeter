ad949ef6ea2782156fcac12447f931efab5839d1
Robert Gates <robert.gates@du.co>
2019-10-22 11:43:23 +0100

    Use Mario font

--- billionmeter.py
+++ billionmeter.py
@@ -37,7 +37,8 @@ ROWS_PER_BALL = 2000000
 NUM_STEPS=45
 
 def update_display(string):
-    font = ImageFont.truetype(FredokaOne, 28)
+    font = ImageFont.truetype("resources/mario.ttf", 28)
+    # font = ImageFont.truetype(FredokaOne, 28)
 
     img = Image.open("resources/inkyphat.png")
     draw = ImageDraw.Draw(img)
