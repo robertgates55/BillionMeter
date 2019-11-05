af807f937f3a352a6fa1100985fe84712b5d1fc0
Robert Gates <robert.gates@du.co>
2019-11-05 16:48:15 +0000

    Fixup

--- billionmeter.py
+++ billionmeter.py
@@ -73,7 +73,7 @@ def getMAC(interface='wlan0'):
 
 
 def override_ball_count(pingpometer_id):
-    request = urllib2.Request(print("https://pingpometer.common.duco.services/%s/get" % pingpometer_id))
+    request = urllib2.Request("https://pingpometer.common.duco.services/%s/get" % pingpometer_id)
 
     base64string = base64.b64encode('%s:%s' % ('duco', os.environ['PINGPOMETER_PASSWORD']))
     request.add_header("Authorization", "Basic %s" % base64string)
