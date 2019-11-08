fe63e36a3dbbf20f4d84069f4e902c4942559319
Robert Gates <robert.gates@du.co>
2019-11-08 10:50:38 +0000

    Add cleaning cycle

--- billionmeter.py
+++ billionmeter.py
@@ -193,3 +193,54 @@ print("Dropping = " + str(num_balls))
 drop_balls(num_balls, latest_row_count)
 
 print("Dropped. New ball count = " + str(get_current_count(BALLS_DROPPED_FILENAME)))
+
+
+
+
+
+
+import time
+import sys
+
+print("""Inky pHAT: Clean
+Displays solid blocks of red, black, and white to clean the Inky pHAT
+display of any screen burn.
+""".format(sys.argv[0]))
+
+if len(sys.argv) < 2:
+    print("""Usage: {} <colour> <number of cycles>
+       Valid colours: red, yellow, black
+""".format(sys.argv[0]))
+    sys.exit(0)
+
+colour = sys.argv[1].lower()
+
+try:
+    inky_display.set_colour(colour)
+except ValueError:
+    print('Invalid colour "{}" for V{}\n'.format(colour, inky_display.get_version()))
+    if inky_display.get_version() == 2:
+        sys.exit(1)
+    print('Defaulting to "red"')
+
+if len(sys.argv) > 2:
+    cycles = int(sys.argv[2])
+else:
+    cycles = 3
+
+colours = (inky_display.RED, inky_display.BLACK, inky_display.WHITE)
+colour_names= (colour, "black", "white")
+
+for i in range(cycles):
+    print("Cleaning cycle %i\n" % (i + 1))
+    for j, c in enumerate(colours):
+        print("- updating with %s" % colour_names[j])
+        inky_display.set_border(c)
+        for x in range(inky_display.WIDTH):
+            for y in range(inky_display.HEIGHT):
+                inky_display.putpixel((x, y), c)
+        inky_display.show()
+        time.sleep(1)
+    print("\n")
+
+print("Cleaning complete!")
